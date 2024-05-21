e8b7dfe0a8c9f9312b564854b80cb78e06a53aa8
JohnVBS <joaovitor.save@hotmail.com>
2024-05-21 15:37:07 -0300

    comitated

--- collections/carros.py
+++ collections/carros.py
@@ -2,22 +2,57 @@
 
 carros = []
 
-menu = input('''CARROS
-1 - Adicionar
-2 - Remover                          
-3 - Listar             
-4 - Verificar se está na lista             
-0 - Sair             
-             ''')
+while True:
+    menu = int(input('''\nCARROS
+    1 - Adicionar
+    2 - Remover                          
+    3 - Listar             
+    4 - Verificar se está na lista             
+    0 - Sair             
+    '''))
 
-match(menu):
-    case 1:
-        print("\nVocê escolheu ADICIONAR!\n")
-        add = input("Adicione o nome de um carro: ")
-    case 2:
+    match(menu):
+        case 1:
+            print("\nVocê escolheu ADICIONAR!\n")
 
-    case 3:
+            add = input("Adicione o nome de um carro: ")
 
-    case 4:
+            if add in carros:
+                print("\nESTE CARRO JA ESTA NA LISTA!")
+                continue
+            else:
+                carros.append(add)
+                print("\nCarro adicionado com sucesso!")
 
-    case 0:
+
+        case 2:
+            print("\nVoce escolheu REMOVER!\n")
+
+            rem = input("Remova o nome de um carro: ")
+
+            if rem in carros:
+                carros.remove(rem)
+                print("\nCarro removido com sucesso!")
+            else:
+                print("\nESTE CARRO NAO ESTA NA LISTA!")
+
+
+        case 3:
+            print("\nVoce escolheu LISTAR!\n")
+
+            print(carros)
+
+
+        case 4:
+            print("\nVoce escolheu VERIFICAR!\n")
+
+            ver = input("Insira o nome de um carro para verificar se esta na lista: ")
+
+            if ver in carros:
+                print("\nESTE CARRO JA ESTA NA LISTA!")
+            else:
+                print("\nESTE CARRO NAO ESTA NA LISTA!")
+
+
+        case 0:
+            break
